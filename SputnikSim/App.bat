872fdb1c868259aee6982513edabe7613e0c0651
Ski's <volocajdaniil0@gmail.com>
2026-02-22 20:06:00 +0500

    Add files via upload

---
+++ SputnikSim/App.bat
@@ -0,0 +1,10 @@
+@echo off
+if not exist venv (
+    python -m venv venv
+)
+call venv\Scripts\activate
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+pip install PyQt5
+python main.py
+pause
