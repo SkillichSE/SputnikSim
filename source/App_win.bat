f9331e415e0ca02ae1b24c35bc9c1a79895ede58
Ski's <volocajdaniil0@gmail.com>
2026-02-27 11:49:01 +0500

    Update App_win.bat

--- source/App_win.bat
+++ source/App_win.bat
@@ -14,8 +14,6 @@ echo  Satellite Orbit Analysis and Simulation System
 echo  ════════════════════════════════════════════════════════════════════════════════
 echo.
 
-@echo off
-chcp 65001 >nul
 cd /d "%~dp0"
 
 if not exist venv (
@@ -24,8 +22,6 @@ if not exist venv (
 )
 
 call venv\Scripts\activate
-pip install -r requirements.txt -q
+python -m pip install -r requirements.txt -q
 python main.py
-pause
-echo  [SYS]   Session ended.
 pause
